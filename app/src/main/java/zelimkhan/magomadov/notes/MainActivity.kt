8a084a9ad9030eb1d78c9e4113fcf413e4d74dc6
Zelimkhan Magomadov <zelimkhan.m21@gmail.com>
2023-08-22 15:51:12 +0300

    Init commit

---
+++ app/src/main/java/zelimkhan/magomadov/notes/MainActivity.kt
@@ -0,0 +1,26 @@
+package zelimkhan.magomadov.notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import zelimkhan.magomadov.notes.ui.theme.NotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
